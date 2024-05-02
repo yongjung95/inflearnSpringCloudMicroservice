e26fcce533653dc8abef3077ecf446abb9073c68
yongjung95 <yongjung95@gmail.com>
2024-05-02 15:07:02 +0700

    :white_check_mark: Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA) : 02. API Gateway Service - Spring Cloud Gateway : Custom Filter 적용

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -19,6 +19,11 @@ public class SecondServiceController {
     @GetMapping("/message")
     public String message(@RequestHeader("second-request") String header) {
         log.info(header);
-        return "Hello World in second Service.";
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from Second service";
     }
 }
