f13acdb65f076715ebc4aaefbcf2a735e751dbdc
yongjung95 <yongjung95@gmail.com>
2024-05-02 14:55:03 +0700

    :white_check_mark: Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA) : 02. API Gateway Service - Spring Cloud Gateway : Filter 적용

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,19 +1,10 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/first-service")
@@ -25,4 +16,9 @@ public class FirstServiceController {
         return "Welcome to the First service.";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
+    }
 }
