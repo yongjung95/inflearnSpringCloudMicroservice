65688c6df2ac180d32ba9244b7f06675e6476cef
yongjung95 <yongjung95@gmail.com>
2024-05-02 13:51:27 +0700

    :tada: Spring Cloud로 개발하는 마이크로서비스 애플리케이션(MSA) : 02. API Gateway Service - Netflix Zuul - 프로젝트 생성 (Deprecated)

---
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,28 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Enumeration;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service.";
+    }
+
+}
